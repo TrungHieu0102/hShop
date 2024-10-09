804e52e9c82f56a2cb65ed80102fcbee91babc21
TrungHieu0102 <hatrunghieu121@gmail.com>
2024-10-09 12:01:40 +0700

    authorize

---
+++ Application/Extentions/RefreshToken.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Extentions
+{
+    public static class RefreshToken
+    {
+        public static string GenerateRefreshToken(int size = 32) // Kích thước mặc định là 32 byte
+        {
+            var randomBytes = new byte[size];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomBytes); // Tạo chuỗi ngẫu nhiên
+            }
+            return Convert.ToBase64String(randomBytes).Replace("+", "").Replace("/", "").Replace("=", ""); // Chuyển đổi thành chuỗi Base64 và loại bỏ ký tự không mong muốn
+        }
+    }
+}
--- Infrastructure/Migrations/20241008093435_AlterTableUser.Designer.cs
+++ Infrastructure/Migrations/20241008093435_AlterTableUser.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(HshopContext))]
-    partial class HshopContextModelSnapshot : ModelSnapshot
+    [Migration("20241008093435_AlterTableUser")]
+    partial class AlterTableUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -190,12 +193,6 @@ namespace Infrastructure.Migrations
 
                     b.Property<bool>("PhoneNumberConfirmed")
                         .HasColumnType("bit");
-
-                    b.Property<string>("RefreshToken")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<DateTime?>("RefreshTokenExpiryTime")
-                        .HasColumnType("datetime2");
 
                     b.Property<string>("SecurityStamp")
                         .HasColumnType("nvarchar(max)");

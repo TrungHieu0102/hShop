a0cfcc28023ab047c28a8e74185d012f8d328128
TrungHieu0102 <hatrunghieu121@gmail.com>
2024-10-19 00:55:13 +0700

    upload image cloudiary

--- WebApi/Controllers/ProductController.cs
+++ WebApi/Controllers/ProductController.cs
@@ -1,12 +1,9 @@
 ﻿using Application.Constants;
 using Application.DTOs.ProductsDto;
 using Application.Interfaces;
-using AutoMapper;
-using Core.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Filters;
-using static Core.SeedWorks.Constants.Permissions;
 
 namespace WebApi.Controllers
 {
@@ -16,22 +13,17 @@ namespace WebApi.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IProductService _productService;
-
         public ProductsController(IProductService productService)
         {
             _productService = productService;
         }
-        [Authorize]
         [HttpGet]
         public async Task<IActionResult> GetProducts([FromQuery] int page = 1, [FromQuery] int pageSize = 10, [FromQuery] string search = "", [FromQuery] bool IsDecsending = false)
         {
             var pagedResult = await _productService.GetAllProductsAsync(page, pageSize, search, IsDecsending);
             return Ok(pagedResult);
         }
-
         [HttpGet("{id}")]
-        [Authorize(Roles = RoleConstants.Admin)]
-
         public async Task<IActionResult> GetProductById(Guid id)
         {
             var result = await _productService.GetProductByIdAsync(id);
@@ -41,21 +33,18 @@ namespace WebApi.Controllers
 
             }
             return Ok(result.Data);
-
         }
         [HttpPost]
-        [Authorize(Products.Create)]
-        public async Task<ActionResult> AddProduct([FromBody] CreateUpdateProductDto productDto)
+        public async Task<ActionResult> AddProduct([FromForm] CreateUpdateProductDto productDto)
         {
-
             try
             {
-                var resutl = await _productService.AddProductAsync(productDto);
-                if (resutl == 0)
+                var result = await _productService.AddProductAsync(productDto);
+                if (result == 0)
                 {
                     return StatusCode(500, "Failed to create product");
                 }
-                return Created();
+                return StatusCode(201, new { message = "Product created successfully", productId = result });
             }
             catch (InvalidOperationException ex)
             {
@@ -67,8 +56,7 @@ namespace WebApi.Controllers
             }
         }
         [HttpPut("{id}")]
-        [Authorize(Roles = RoleConstants.Admin)]
-        public async Task<IActionResult> UpdateProduct(Guid id, [FromBody] CreateUpdateProductDto productDto)
+        public async Task<IActionResult> UpdateProduct(Guid id, [FromForm] CreateUpdateProductDto productDto)
         {
             var result = await _productService.UpdateProductAsync(id, productDto);
             if (!result.IsSuccess)
@@ -77,7 +65,6 @@ namespace WebApi.Controllers
             }
             return Ok();
         }
-        [Authorize(Roles = RoleConstants.Admin)]
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteProduct(Guid id)
         {
